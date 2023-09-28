0893caf534a62e16bd47719e508eb169996680bb
Gabriel <Gabriel.d.owen-1@ou.edu>
2023-09-28 16:39:42 -0500

    Changed Comments

--- FlaskEnvironment/FlaskTest.py
+++ FlaskEnvironment/FlaskTest.py
@@ -1,8 +1,9 @@
 # This file is used to test if flask is working on your machine
 
 # RUN THESE COMMANDS IN CMD LINE:
-# 1: export FLASK_APP=FlaskTest.py
-# 2: flask run
+# 1: Step into FlaskEnvironment directory
+# 2: export FLASK_APP=FlaskTest.py
+# 3: flask run
 # then paste URL into browser to see it running
 
 
@@ -10,4 +11,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    return 'This is Flask at work!'
