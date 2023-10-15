795f97a9b4aaa9628b2ea06c4f6dc0c5de92a3af
bons0002 <sam_bons@yahoo.com>
2023-10-15 07:47:11 -0500

    Turn into various functions

--- image-gen/decode.py
+++ image-gen/decode.py
@@ -2,22 +2,27 @@
 import json
 from base64 import b64decode
 from pathlib import Path
-import sys
 
-# Get the directory with the json files
-DATA_DIR = Path.cwd() / "json_responses"
-# Pass the filename at runtime
-JSON_FILE = DATA_DIR / sys.argv[1]
-IMAGE_DIR = Path.cwd() / "images"
+# Decode the base64 string from a json file to a png
+def decode(JSON_FILE, is_multi):
+    # is_multi indicates whether it is a multi object image or not
+    # If it is multi object, save it to a 'multi' subdirectory
+    if is_multi == True:
+        IMAGE_DIR = Path.cwd() / "images" / "multi"
+    # Else, save it to a 'single' subdirectory
+    else:
+        IMAGE_DIR = Path.cwd() / "images" / "single"
 
-# Create an images directory if one doesn't exist
-IMAGE_DIR.mkdir(parents=True, exist_ok=True)
+    # Create an images directory if one doesn't exist
+    IMAGE_DIR.mkdir(parents=True, exist_ok=True)
 
-with open(JSON_FILE, mode="r", encoding="utf-8") as file:
-    response = json.load(file)
+    # Load in the json file
+    with open(JSON_FILE, mode="r", encoding="utf-8") as file:
+        response = json.load(file)
 
-for index, image_dict in enumerate(response["data"]):
-    image_data = b64decode(image_dict["b64_json"])
-    image_file = IMAGE_DIR / f"{JSON_FILE.stem}-{index}.png"
-    with open(image_file, mode="wb") as png:
-        png.write(image_data)
+    # Decode the base64 to png
+    for index, image_dict in enumerate(response["data"]):
+        image_data = b64decode(image_dict["b64_json"])
+        image_file = IMAGE_DIR / f"{JSON_FILE.stem}-{index}.png"
+        with open(image_file, mode="wb") as png:
+            png.write(image_data)
--- image-gen/image-gen.py
+++ image-gen/image-gen.py
@@ -1,34 +1,86 @@
 # This is a script for generating images with the OpenAI DALL E api
+from decode import decode   # User create library
 import json
 import os
 import openai
 from pathlib import Path
+import sys
 
 # List of objects that will appear in the images
-objects = ["a car", "a dog", "a rocket", "a computer", "a t-rex", "Abraham Lincoln"]
+objects = ["a car", "a dog", "a rocket", "a computer", "a t-rex", "a map"]
 # List of backgrounds for the images
-backgrounds = ["ocean", "outer space", "rainforest", "city"]
+backgrounds = ["the ocean", "outer space", "a rainforest", "a city"]
+# List of different styles
+styles = ["cartoonish", "historic", "surreal"]
 
-# The prompt for the image generation
-PROMPT = f"an image of {objects[3]} and {objects[5]} with a background of {backgrounds[1]}"
-# The directory to store the json responses
-DATA_DIR = Path.cwd() / "json_responses"
-# Make the directory if it doesn't exist
-DATA_DIR.mkdir(exist_ok=True)
-
-# The environment variable for the OpenAI api key stored on local machine
+# The environment variable for the OpenAI api key stored on local machine (On POSIX, use `export OPENAI_API_KEY="<your-key-value-here>"`)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# The json response for the generated image
-response = openai.Image.create(
-    prompt=PROMPT,
-    n=1,
-    size="256x256",
-    response_format = "b64_json",
-)
+# Generate images with three objects in them
+def generate_multiobj(num_iter, idxs, idx1, idx2, idx3, idxbg):
+    # The prompt for the image generation
+    PROMPT = f"A {styles[idxs]} image containing {objects[idx1]}, {objects[idx2]}, and {objects[idx3]} located in {backgrounds[idxbg]}."
+    # The directory to store the json responses for multi obj images
+    DATA_DIR = Path.cwd() / "json_responses" / "multi"
+    # Make the directory if it doesn't exist
+    DATA_DIR.mkdir(exist_ok=True)
 
-# The name of the json file that will be saved
-file_name = DATA_DIR / f"{PROMPT[:50]}-{response['created']}.json"
-# Write the json response to a file
-with open(file_name, mode="w", encoding="utf-8") as file:
-    json.dump(response,file)
+    # Loop to generate num_iter number of images
+    for i in range(0, num_iter):
+        # The json response for the generated image
+        response = openai.Image.create(
+            prompt=PROMPT,
+            n=1,
+            size="256x256",
+            response_format = "b64_json",
+        )
+
+        # The name of the json file that will be saved
+        filename = DATA_DIR / f"{PROMPT[:50]}-{response['created']}.json"
+        # Write the json response to a file as base64
+        with open(filename, mode="w", encoding="utf-8") as file:
+            json.dump(response,file)
+        # Decode the base64 image in the json to a png (with flag to indicate its a multi obj image)
+        decode(filename, True)
+
+# Generate images with only one object
+def generate_singleobj(num_iter, idxs, idx1, idxbg):
+    # The prompt for the image generation
+    PROMPT = f"A {styles[idxs]} image containing {objects[idx1]} located in {backgrounds[idxbg]}."
+    # The directory to store the json responses for single obj images
+    DATA_DIR = Path.cwd() / "json_responses" / "single"
+    # Make the directory if it doesn't exist
+    DATA_DIR.mkdir(exist_ok=True)
+
+    # Loop to generate num_iter number of images
+    for i in range(0, num_iter):
+        # The json response for the generated image
+        response = openai.Image.create(
+            prompt=PROMPT,
+            n=1,
+            size="256x256",
+            response_format = "b64_json",
+        )
+
+        # The name of the json file that will be saved
+        filename = DATA_DIR / f"{PROMPT[:75]}-{response['created']}.json"
+        # Write the json response to a file as base64
+        with open(filename, mode="w", encoding="utf-8") as file:
+            json.dump(response,file)
+        # Decode the base64 image in the json to a png (without flag for multi obj image)
+        decode(filename, False)
+
+
+# Get arguments passed at runtime and cast to integers
+argv = sys.argv[1:]
+args = [eval(i) for i in argv]
+
+# If there are 6 runtime args, generate multi object images
+if len(args) == 6:
+    generate_multiobj(args[0], args[1], args[2], args[3], args[4], args[5])
+# If 4 runtime args, generate a single object image
+elif len(args) == 4:
+    generate_singleobj(args[0], args[1], args[2], args[3])
+# If invalid number of runtime args, print an error
+else:
+    print("Invalid number of arguments", file=sys.stderr)
