34b4fcf06c186e2eb16aa223645d799b08319bc2
bons0002 <sam_bons@yahoo.com>
2023-12-01 16:10:19 -0600

    Implement select_clues function

--- FlaskTest.py
+++ FlaskTest.py
@@ -8,7 +8,9 @@
 
 import os
 import random
+from PIL import Image
 
+clues = [] # Paths to the two selected clues
 solution = []
 
 # Select the two objects that will be used in the CAPTCHA
@@ -21,6 +23,16 @@ def select_objects():
     if obj1 < obj2:
         return [obj1, obj2]
     return [obj2, obj1]
+
+# Select the images that will be used in the CAPTCHA
+def select_clues():
+    objs = select_objects() # Select the objects
+    clue1_dir = f'static/images/single/{objs[0]}'   # Path to directory of first clue options
+    clue2_dir = f'static/images/single/{objs[1]}'   # Path to dir of second clue options
+    single_imgs1 = [f for f in os.listdir(clue1_dir) if f.endswith('.png')] # Get all images in clue1_dir
+    single_imgs2 = [f for f in os.listdir(clue2_dir) if f.endswith('.png')] # Get all images in clue2_dir
+    clues.append(f'images/single/{objs[0]}/{single_imgs1[random.randint(0, len(single_imgs1))]}')    # Get random clue 1
+    clues.append(f'images/single/{objs[1]}/{single_imgs2[random.randint(0, len(single_imgs2))]}')    # Get random clue 2
 
 def generate_captcha():
     image_files = [f for f in os.listdir('static/NineImageTest') if f.endswith('.png')]
