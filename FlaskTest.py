3170ffea69e8fb62bc52906eff2e9eed1d12b1fb
Gabriel <Gabriel.d.owen-1@ou.edu>
2023-12-02 13:11:34 -0600

    pass form values to back end

--- FlaskTest.py
+++ FlaskTest.py
@@ -8,10 +8,12 @@
 
 import os
 import random
+from unittest import result
 from PIL import Image
 
 clues = [] # Paths to the two selected clues
 solution = []
+
 
 # Select the two objects that will be used in the CAPTCHA
 def select_objects():
@@ -24,6 +26,7 @@ def select_objects():
         return [obj1, obj2]
     return [obj2, obj1]
 
+
 # Select the images that will be used in the CAPTCHA
 def select_clues():
     objs = select_objects() # Select the objects
@@ -33,6 +36,7 @@ def select_clues():
     single_imgs2 = [f for f in os.listdir(clue2_dir) if f.endswith('.png')] # Get all images in clue2_dir
     clues.append(f'images/single/{objs[0]}/{single_imgs1[random.randint(0, len(single_imgs1))]}')    # Get random clue 1
     clues.append(f'images/single/{objs[1]}/{single_imgs2[random.randint(0, len(single_imgs2))]}')    # Get random clue 2
+
 
 def generate_captcha():
     image_files = [f for f in os.listdir('static/NineImageTest') if f.endswith('.png')]
@@ -47,14 +51,17 @@ def generate_captcha():
 
 # Flask stuff
 
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, url_for, redirect
+
 app = Flask(__name__)
 if __name__ =='__main__':
     app.run(debug=True)
 
+
 @app.route('/')
 def home():
-    #return render_template("index.html")   
+    # return render_template("index.html")
     captcha_code, selected_images, correct_index = generate_captcha()
     if not captcha_code:
         return 'Not enough captcha images found.'
@@ -62,8 +69,11 @@ def home():
     return render_template('index.html', captcha_code=captcha_code, selected_images=selected_images,
                            correct_index=correct_index)
 
+
 @app.route('/validate', methods=['POST'])
 def validate():
     # Your validation logic here
+    print(request.form)
     print("validating")
-    pass
+    
+    return redirect(url_for('home'))
