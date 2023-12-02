a1a0a8b282774384e4cf15fe3ad2ce16fe4ad16e
bons0002 <sam_bons@yahoo.com>
2023-12-02 14:50:09 -0600

    Remove global variables

--- FlaskTest.py
+++ FlaskTest.py
@@ -10,10 +10,6 @@ import os
 import random
 from PIL import Image
 
-objs = []   # The values for the correct objects
-clues = [] # Paths to the two selected clues
-images = [] # List of images in grid
-solution = []
 
 # Select the two objects that will be used in the CAPTCHA
 def select_objects():
@@ -25,22 +21,21 @@ def select_objects():
     if obj1 < obj2:
         return [obj1, obj2]
     return [obj2, obj1]
+# End select_objects
 
 # Select the images that will be used in the CAPTCHA
-def select_clues():
-    global objs # Use the global variable for objs
-    global clues    # Use the global variable
-    objs = select_objects() # Select the objects
+def select_clues(objs):
+    clues = []
     clue1_dir = f'static/images/single/{objs[0]}'   # Path to directory of first clue options
     clue2_dir = f'static/images/single/{objs[1]}'   # Path to dir of second clue options
     single_imgs1 = [f for f in os.listdir(clue1_dir) if f.endswith('.png')] # Get all images in clue1_dir
     single_imgs2 = [f for f in os.listdir(clue2_dir) if f.endswith('.png')] # Get all images in clue2_dir
     clues.append(f'images/single/{objs[0]}/{single_imgs1[random.randint(0, len(single_imgs1) - 1)]}')    # Get random clue 1
     clues.append(f'images/single/{objs[1]}/{single_imgs2[random.randint(0, len(single_imgs2) - 1)]}')    # Get random clue 2
+    return clues
+# End select_clues
 
-def select_images():
-    global images
-    select_clues()
+def select_images(objs, clues):
     prefix = "images/multi" # Prefix for directory
     all_dirs = os.listdir(f'static/{prefix}')   # Get all directories in 'static/images/multi/'
 
@@ -82,7 +77,7 @@ def select_images():
             if inc_imgs2[rand] not in incorrect:
                 incorrect.append(f'{prefix}/{incorrect_dirs[1]}/{inc_imgs2[rand]}')
 
-    images = correct + incorrect
+    return correct + incorrect
 # End select_images
 
 def generate_captcha():
@@ -118,3 +113,8 @@ def validate():
     # Your validation logic here
     print("validating")
     pass
+
+# TEST CODE
+objs = select_objects()
+clues = select_clues(objs)
+print(select_images(objs, clues))
